f5fa979dbdf1e589b1823bb03b8cad8fe6fa1687
Braxton Shoop <braxton.shoop@gmail.com>
2020-12-03 14:39:54 -0600

    initial roommates example setup completed

---
+++ Roommates/Models/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roommates.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MaxOccupancy { get; set; }
+    }
+
+
+}
+
